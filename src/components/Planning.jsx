898bd675efc9a26a90aa2218f179e9681208cf3d
BastienVienet <bastien.vienet@outlook.com>
2023-02-22 08:41:12 +0100

    using map function to display data fetch from API

--- src/components/Planning.jsx
+++ src/components/Planning.jsx
@@ -6,7 +6,7 @@ export const Planning = ({ userInfos }) => {
 
     const fetchCours = async () => {
 
-        const response = await fetch('http://15.188.53.208:8080/utgGETPlanningCours/2',
+        const response = await fetch('http://15.188.53.208:8080/utgGETPlanningCours/1',
         {
             method: 'POST',
             headers: { 'Content-Type': 'application/json; charset=utf-8' },
@@ -18,7 +18,12 @@ export const Planning = ({ userInfos }) => {
     }
 
     fetchCours()
-    
+    const coursDataWithFormattedDate = coursData.map((cours) => {
+        const date = new Date(cours.Date_session);
+        const formatedDate = date.toLocaleDateString('fr-FR', { day: '2-digit', month: '2-digit', year: 'numeric' }).replace(/\//g, '.');
+        return { ...cours, formatedDate };
+    });
+
     return (
         <div className="m-6">
         <h1 className="title is-spaced has-text-weight-normal">Planification</h1>
@@ -31,9 +36,9 @@ export const Planning = ({ userInfos }) => {
             </tr>
             </thead>
             <tbody>
-            {coursData.map((cours, index) => (
+            {coursDataWithFormattedDate.map((cours, index) => (
                         <tr key={index}>
-                            <td>{cours.Date_session}<br/>{cours.Session}</td>
+                            <td>{cours.formatedDate}<br/>{cours.Session}</td>
                             <td>{cours.Nom_du_cours}<br/>{cours.Formateur}</td>
                             <td>{cours.Lieu_du_cours}</td>
                         </tr>
