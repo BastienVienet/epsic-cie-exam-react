cfbd127d0965405036514c1fca8ea435704d8ac4
BastienVienet <bastien.vienet@outlook.com>
2023-02-26 15:57:56 +0100

    Adding react-icons and react router dom

---
+++ src/components/Menu.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+
+export const Menu = () => {
+
+    return(
+        <nav className="navbar">
+            <div>
+                <Link to="/" className="navbar-item">Planning</Link>
+                <Link to="/portfolio" className="navbar-item">Portfolio</Link>
+            </div>
+        </nav>
+    )
+}
